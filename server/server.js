4ca53eb587f59869ca4ae7eb93e1e5ae25aa75ae
Subham Gautam <gautamsubham65@gmail.com>
2026-03-27 17:50:24 +0545

    basic backend setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀")
+})
+
+app.get("/hello",(req,res)=>{
+    res.send("hello subham");
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
