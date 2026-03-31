c33f83661140871e72d81cd2c44fcd80bdfda11f
Subham Gautam <gautamsubham65@gmail.com>
2026-03-31 23:43:08 +0545

    prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`)
 // })
+import dotenv from "dotenv"
+dotenv.config()
 import express from "express"
 import cors from "cors"
 import connectDB from "./config/db.js"
